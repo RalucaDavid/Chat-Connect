5cd2141a912c703fa20827a9f8a4f5c852e71a9c
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2024-08-15 23:27:27 +0300

    add mantine provider

--- Frontend/chat-connect/src/index.tsx
+++ Frontend/chat-connect/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { MantineProvider } from '@mantine/core';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
 
