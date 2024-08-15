70565487afbff72472494ab35aeb7a9677df2694
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2024-08-16 00:16:44 +0300

    change the icon from the tab

--- Frontend/chat-connect/src/App.tsx
+++ Frontend/chat-connect/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from '@mantine/core';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+
     </div>
   );
 }
